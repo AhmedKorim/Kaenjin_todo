45e0e2618f385bff1aabafccb3325be7de6dd7d5
ahmedkorim <ahmedkorrim@gmail.com>
2018-11-17 11:03:52 +0200

    login register routes

--- api/routes/user.js
+++ api/routes/user.js
@@ -8,101 +8,90 @@ const User = require('../../models/User');
 const router = Express.Router();
 const {secretOrKey} = require('../../config/keys');
 
-router.post('/register', (req, res, next) => {
 
-    User.findOne({email: req.body.email})
-        .then(doc => {
-            if (doc) {
-                return res.status(400)
-                    .json({
-                        error: 'this email is already in use'
-                    })
-            }
-            // creating new user
+router.post('/register', async (req, res, next) => {
+    const {email, password, name} = req.body;
+    try {
+        const user = await User.findOne({email})
+        if (user) {
+            return res.status(400)
+                .json({
+                    error: 'this email is in use already'
+                })
+        } else {
             const newUser = new User({
-                email: req.body.email,
-                name: req.body.name,
-                password: req.body.password
-            });
+                email,
+                name,
+                password
+            })
             bcrypt.genSalt(10, (err, salt) => {
-                return bcrypt.hash(newUser.password, salt, (err, hashedPassword) => {
-                    if (err) {
-                        console.log(err);
-                        return res.status(500)
+                bcrypt.hash(newUser.password, salt, async (err, hashPassword) => {
+                    if (err) return res.status(500).json({error: 'internal server error'})
+
+                    newUser.password = hashPassword;
+                    const user = await newUser.save();
+                    if (user) {
+                        res.status(200)
                             .json({
-                                error: "internal server error"
-                            })
-                    }
-                    newUser.password = hashedPassword;
-                    return newUser.save()
-                        .then(doc => {
-                            console.log(doc);
-                            if (doc) return res.status(201).json({
                                 message: 'user created',
                                 user: {
-                                    name: doc.name,
+                                    name: user.name,
                                 }
                             })
-                        })
+                    }
                 })
             })
-                .catch(err => console.log(err))
-        })
+        }
+    } catch (e) {
+        res.status(500).json({message:'internal server error'});
+    }
 
 })
-router.post('/login', (req, res) => {
+
+
+router.post('/login', async (req, res) => {
     const {email, password} = req.body;
 
-
-    User.findOne({email})
-        .then(user => {
-            if (!user) {
-                return res.status(404).json({
-                    error: 'email is not found'
-                })
+    try {
+        const user = await User.findOne({email})
+        if (!user) {
+            return res.status(404).json({
+                error: 'email is not found'
+            })
+        }
+        const isMatch = await bcrypt.compare(password, user.password)
+        if (isMatch) {
+            const payload = {
+                id: user.id,
+                name: user.name,
+                email: user.email,
+                expTimeout: 3600 * 3,
             }
+            jwt.sign(payload, secretOrKey, {expiresIn: 3600 * 3}, (err, token) => {
+                if (err) return res.status(500).json({error: 'internal Server Error'})
 
-           return bcrypt.compare(password, user.password)
-                .then(isMatch => {
-                    if (isMatch) {
-                        const payload = {
-                            id: user.id,
-                            name: user.name,
-                            email: user.email,
-                            expTimeout: 3600 * 3,
-                            date: Date.now()
-                        }
+                res.status(200).json({
+                    message: "success login",
+                    user: {
+                        id: user._id.toString(),
+                        userName: user.name,
+                    },
+                    token: 'Bearer ' + token
+                })
+            })
+        } else {
+            res.status(401)
+                .json({
+                    message: 'failed to login'
+                })
+        }
+    } catch (e) {
+        console.log(e);
+        res.status(500)
+            .json({
+                message: "internal serve error"
+            })
+    }
 
-
-                      return  jwt.sign(payload, secretOrKey,
-                          {expiresIn: 3600 * 3},
-                            (err, token) => {
-                          res.status(200)
-                                    .json({
-                                        message: "success login",
-                                        user: {
-                                            id: user._id.toString(),
-                                            userName: user.name,
-                                        },
-                                        token: 'Bearer ' + token
-                                    })
-
-                            })
-
-                    } else {
-                        return res.status(400)
-                            .json({
-                                message: 'login failed'
-                            })
-                    }
-                }).catch(err =>
-                console.log(err))
-        })
-        .then(err => {
-            console.log(err);
-
-            res.status(500)
-                .json({error: 'internal server error'})
-        })
 })
 module.exports = router;
